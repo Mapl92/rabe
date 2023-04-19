88fa07d64160a0198b495e6b7b44b0f839c6d6c4
Maximilian Stör <mstoer@airvonmximilian.speedport.ip>
2023-04-19 22:17:54 +0200

    gunicorn2

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,4 +3,4 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
 worker_class = "uvicorn.workers.UvicornWorker"
-module = "rabe:app"
+wsgi_app = "rabe:app"
